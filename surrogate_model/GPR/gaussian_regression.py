1453f6779669bada040d9e71cbf8c16e42e34278
James <tomas.monopoli@gmail.com>
2024-03-10 13:39:48 +0100

    new version of the gaussian regression class. Several changes to enhance simplicity, readability and maintainability

--- surrogate_model/GPR/gaussian_regression.py
+++ surrogate_model/GPR/gaussian_regression.py
@@ -1,124 +1,187 @@
 from typing import Union, List, Tuple, Optional, Callable
 from copy import deepcopy
+from dataclasses import dataclass
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.gaussian_process.kernels import Kernel
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF, Matern, ConstantKernel as C
 
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
-from my_packages.constants import (
-    FieldComponents,
-    FieldTypes,
-    Orientations,
-    ProbeTypes,
-    DistanceUnits,
-)
-
-from .custom_scikit_kernel import MyRBF, StepKernel
+from my_packages.constants import DistanceUnits
 from .kernel_manager import KernelManager   
 
 from ..abstract_sample_model import AbstractSampleModel
 from ..surrogate_model import SurrogateModel
 
-from my_packages.EM_fields.scans import Scan
-from my_packages.classes.aux_classes import Grid
 
+@dataclass
+class GaussianRegressionModelConfig:
+    normalization_method: str = "standard"
+    max_range: float = None
+    units: DistanceUnits = DistanceUnits.mm
+    n_restarts_optimizer: int = 10
+    alpha: float = 1e-10
+    kernel_base_function: Callable = None
+    kernel_kwargs: dict = None
+    kernel_non_normalize_hyperparams: dict = None
+
+    def __post_init__(self):
+        if self.kernel_kwargs is None:
+            self.kernel_kwargs = {}
+        if self.kernel_non_normalize_hyperparams is None:
+            self.kernel_non_normalize_hyperparams = {}
+        if isinstance(self.units, str):
+            self.units = DistanceUnits[self.units]
 
 class GaussianRegressionModel(AbstractSampleModel):  
+    """
+    A wrapper class for Gaussian Process Regression using scikit-learn's GaussianProcessRegressor.
+    Provides methods for fitting, predicting, and evaluating the Gaussian Process model.
+    """
     def __init__(
         self,
-        normalization_method: str = "standard",
-        max_range: float = None,
-        units: DistanceUnits = DistanceUnits.mm,
-        n_restarts_optimizer: int = 10,
-        alpha: float = 1e-10,
-        kernel_base_function: Callable = None,
-        kernel_kwargs: dict = None,
-        kernel_non_normalize_hyperparams: dict = None,
+        configs: GaussianRegressionModelConfig = GaussianRegressionModelConfig(),
+        **kwargs
     ):
-        if isinstance(units, str):
-            units = DistanceUnits[units]
+        # if there are kwargs
+        for key, value in kwargs.items():
+            setattr(configs, key, value)
+        # run the post init method again
+        configs.__post_init__()
+        self.configs = configs    
 
-        self.n_restarts_optimizer = n_restarts_optimizer
-        self.max_range = max_range
-        self.alpha = alpha
-        self.units = units
-        self._set_normalization_scaler(method=normalization_method)
-        
-        if kernel_base_function is None:
+        self._set_normalization_scaler(method=configs.normalization_method)
+
+        if configs.kernel_base_function is None:
             self.kernelM = KernelManager()
         else:
             self.kernelM = KernelManager(
-                kernel_function=kernel_base_function,
-                kernel_params=kernel_kwargs,
-                non_normalize_params=kernel_non_normalize_hyperparams,
+                kernel_function=configs.kernel_base_function,
+                kernel_params=configs.kernel_kwargs,
+                non_normalize_params=configs.kernel_non_normalize_hyperparams,
             )
-    
+
+    @property
+    def units(self):
+        return self.configs.units
+
+    @property
+    def n_restarts_optimizer(self):
+        return self.configs.n_restarts_optimizer
+
+    @n_restarts_optimizer.setter
+    def n_restarts_optimizer(self, value):
+        self.configs.n_restarts_optimizer = value
+
+    @property
+    def alpha(self):
+        return self.configs.alpha
+    @alpha.setter
+    def alpha(self, value):
+        self.configs.alpha = value
+
+    @property
+    def max_range(self):
+        return self.configs.max_range
+    @max_range.setter
+    def max_range(self, value):
+        self.configs.max_range = value
+
+    @property
+    def normalization_method(self):
+        return self.configs.normalization_method
+    @normalization_method.setter
+    def normalization_method(self, value):
+        self.configs.normalization_method = value
+        self._set_normalization_scaler(value)
+
     @property
     def surrogate_model(self):
         return SurrogateModel(self)
-
-    def update_kernel(self, base_kernel: Callable = None, non_normalize_hyperparams: bool = None, **kwargs)-> "GaussianRegressionModel":
-        if non_normalize_hyperparams is None:
-            if base_kernel is None:
-                non_normalize_hyperparams = KernelManager.DEFAULT_NON_NORMALIZE_PARAMS.copy()
-            else:
-                non_normalize_hyperparams = {}
-            
-        if base_kernel is None:
-            base_kernel = KernelManager.DEFAULT_KERNEL_FUNCTION
-        
-        kernel_base_function = base_kernel
-        kernel_kwargs = kwargs
-        non_normalize_hyperparams = non_normalize_hyperparams
-        
-        self.kernelM = KernelManager(
-            kernel_function=kernel_base_function,
-            kernel_params=kernel_kwargs,
-            non_normalize_params=non_normalize_hyperparams,
-        )
-        return self        
-
-    def _set_normalization_scaler(self, method="standard"):
-        if method == "standard":
-            self.scaler = StandardScaler()
-        elif method == "minmax":
-            self.scaler = MinMaxScaler()
-        else:
-            self.scaler = None
+    
+    def update_configs_properties(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self.configs, key, value)
+        self.configs.__post_init__()
         return self
 
-    def fit(self, X, y, n_restarts_optimizer=None, alpha=None, max_range=None, **kwargs) -> "GaussianRegressionModel":
+    def update_kernel(
+        self, base_kernel: Callable = None, non_normalize_hyperparams: dict = None, **kwargs
+    ) -> "GaussianRegressionModel":
+        """
+        Updates the kernel of the Gaussian Regression model.
+
+        Args:
+            base_kernel (Callable, optional): The base kernel function to use. Defaults to None.
+            non_normalize_hyperparams (dict, optional): Non-normalized hyperparameters for the kernel. Defaults to None.
+            **kwargs: Additional keyword arguments for the kernel function.
+
+        Returns:
+            GaussianRegressionModel: The updated Gaussian Regression model.
+
+        """
+        base_kernel = base_kernel or KernelManager.DEFAULT_KERNEL_FUNCTION
+        non_normalize_hyperparams = (
+            non_normalize_hyperparams or KernelManager.DEFAULT_NON_NORMALIZE_PARAMS.copy()
+        )
+
+        self.kernelM = KernelManager(
+            kernel_function=base_kernel,
+            kernel_params=kwargs,
+            non_normalize_params=non_normalize_hyperparams,
+        )
+        return self
+
+    def _set_normalization_scaler(self, method: str):
+        scalers = {
+            "standard": StandardScaler(),
+            "minmax": MinMaxScaler(),
+        }
+        self.scaler = scalers.get(method)
+
+    def _preprocess_data(self, X: np.ndarray, fit=False) -> np.ndarray:
+        X = X / self.units.value
+        if self.scaler:
+            if fit:
+                return self.scaler.fit_transform(X)
+            else:
+                return self.scaler.transform(X)
+            
+    def fit(self, X, y, **kwargs) -> "GaussianRegressionModel":
         """
         Fit the Gaussian Process model.
         :param X: 2D array of spatial coordinates (shape [n_samples, 2]).
         :param y: Observed field values at these coordinates (shape [n_samples]).
         """
-        if n_restarts_optimizer is None:
-            n_restarts_optimizer = self.n_restarts_optimizer
-        if alpha is None:
-            alpha = self.alpha
 
         # change the units of the spatial coordinates
-        X = X / self.units.value
-        X_scaled = self.scaler.fit_transform(X)
-        
+        X_scaled = self._preprocess_data(X, fit=True)
         self.kernelM.set_scaler(self.scaler)
-        self.kernel = self.kernelM.make_kernel(normalize=True, max_range=max_range)
+        self.kernel = self.kernelM.make_kernel(
+            normalize=True, max_range=self.max_range
+            )
         # self.kernel = self.make_kernel(normalize=True, max_range=max_range)
-        
+
         self.gp = GaussianProcessRegressor(
             kernel=self.kernel,
-            n_restarts_optimizer=n_restarts_optimizer,
-            alpha=alpha,
+            n_restarts_optimizer=self.n_restarts_optimizer,
+            alpha=self.alpha,
             **kwargs,
         )
         self.gp.fit(X_scaled, y)
         return self
-        
+
+    def predict(self, X: np.ndarray, return_std: bool = False) -> Union[np.ndarray, Tuple[np.ndarray, np.ndarray]]:
+        """
+        Make predictions using the fitted model.
+        :param X: 2D array of spatial coordinates for prediction (shape [n_samples, 2]).
+        :param return_std: If True, returns the standard deviation of the predictions.
+        :return: Predicted field values at the given coordinates (shape [n_samples]).
+                 If return_std is True, also returns the standard deviation of the predictions.
+        """
+        X_scaled = self._preprocess_data(X, fit=False)
+        return self.gp.predict(X_scaled, return_std=return_std)
 
     def extract_theta(self):
         return self.gp.kernel_.theta
@@ -128,39 +191,24 @@ class GaussianRegressionModel(AbstractSampleModel):
         self.kernel = self.kernel.clone_with_theta(params)
         return self.kernel
 
-    def fit_from_theta(self, X, y, theta: dict, K0: Kernel, n_restarts_optimizer=None, alpha=None, max_range=None, **kwargs)-> "GaussianRegressionModel":
+    def fit_from_theta(self, X, y, theta: dict, K0: Kernel, **kwargs)-> "GaussianRegressionModel":
         """
         Fit the Gaussian Process model.
         :param X: 2D array of spatial coordinates (shape [n_samples, 2]).
         :param y: Observed field values at these coordinates (shape [n_samples]).
         """
-        if n_restarts_optimizer is None:
-            n_restarts_optimizer = self.n_restarts_optimizer
-        if alpha is None:
-            alpha = self.alpha
         # change the units of the spatial coordinates
-        X = X / self.units.value
-        X_scaled = self.scaler.fit_transform(X)
+        X_scaled = self._preprocess_data(X, fit=True)
         self.kernel = self.set_kernel_theta(theta, K0)
         self.gp = GaussianProcessRegressor(
             kernel=self.kernel,
-            n_restarts_optimizer=n_restarts_optimizer,
-            alpha=alpha,
-            **kwargs,
+            n_restarts_optimizer=self.n_restarts_optimizer,
+            alpha=self.alpha,
+            **kwargs
         )
         self.gp.fit(X_scaled, y)
         return self
 
-    def predict_with_std(self, X) -> Tuple[np.ndarray, np.ndarray]:
-        """
-        Make predictions using the fitted model.
-        :param X: 2D array of spatial coordinates for prediction.
-        :return: Predicted field values at these coordinates and the std of the model.
-        """
-        # change the units of the spatial coordinates
-        X = X / self.units.value
-        X = self.scaler.transform(X)
-        return self.gp.predict(X, return_std=True)
 
     def prediction_std(self, X) -> np.ndarray:
         """
@@ -173,48 +221,32 @@ class GaussianRegressionModel(AbstractSampleModel):
         X = self.scaler.transform(X)
         return self.gp.predict(X, return_std=True)[1]
     
-    def predict(self, X) -> np.ndarray:
+    def get_kernel_params(self) -> dict:
         """
-        Make predictions using the fitted model.
-        :param X: 2D array of spatial coordinates for prediction.
-        :return: Predicted field values at these coordinates and the std of the model.
-        """
-        # change the units of the spatial coordinates
-        X = X / self.units.value
-        X = self.scaler.transform(X)
-        return self.gp.predict(X)
-
-    def get_final_length_scales(self):
-        """
-        Retrieve the final length scales used in the Gaussian Process model.
-        :return: Length scales as a numpy array.
+        Retrieve the kernel parameters (length scales and bounds).
+        :return: Dictionary containing the length scales and bounds.
         """
         length_scales = []
-        if self.gp.kernel_ is not None:
-            for hyp in self.gp.kernel_.get_params():
-                if "length_scale" in hyp and "bounds" not in hyp:
-                    length_scale = deepcopy(self.gp.kernel_.get_params()[hyp])
-                    length_scales.append(length_scale)
-                # if "length_scale_bounds" in hyp:
-                #     length_scale_bounds = deepcopy(self.gp.kernel_.get_params()[hyp])
-            
-            # denormalize length scale
-            length_scales = np.asarray(length_scales) * self.scaler.scale_[0]
-            # length_scale_bounds = tuple(np.asarray(length_scale_bounds) * self.scaler.scale_[0])    
-        return length_scales
-    
-    def get_length_scale_bounds(self):
-        all_length_scale_bounds = []
-        if self.gp.kernel_ is not None:
-            for hyp in self.gp.kernel_.get_params():
-                if "length_scale_bounds" in hyp:
-                    length_scale_bounds = deepcopy(self.gp.kernel_.get_params()[hyp])
-                    all_length_scale_bounds.append(np.asarray(length_scale_bounds))
-            # denormalize length scale
-            length_scale_bounds = np.asarray(all_length_scale_bounds) * self.scaler.scale_[0]
-        return length_scale_bounds
-    
+        length_scale_bounds = []
 
+        for param_name, param_value in self.gp.kernel_.get_params().items():
+            if 'length_scale' in param_name:
+                if 'bounds' not in param_name:
+                    length_scales.append(param_value)
+                else:
+                    length_scale_bounds.append(param_value)
+
+        if self.scaler:
+            length_scales = np.asarray(length_scales) * self.scaler.scale_[0]
+            length_scale_bounds = np.asarray(length_scale_bounds) * self.scaler.scale_[0]
+
+        return {
+            'length_scales': length_scales,
+            'length_scale_bounds': length_scale_bounds,
+        }
+
+
+class OtherGaussianRegressionModel(GaussianRegressionModel):
     def is_data_stationary(self, X, y, num_segments=4, threshold=0.05) -> Tuple[bool, bool]:
         """
         Check if the data is stationary, in terms of mean and variance.
